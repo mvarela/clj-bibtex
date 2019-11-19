a3b5b70d43e186842356eaeb3060935348f00dd9
Martín Varela <martin@varela.fi>
2019-11-19 09:50:35 +0200

    Simplified author processing, added conversion and queries

--- src/fi/varela/bibtex_db.clj
+++ src/fi/varela/bibtex_db.clj
@@ -1,52 +1,76 @@
 (ns fi.varela.bibtex-db
   (:require [datascript.core :as d]))
 
-(def conn (d/conn-from-db
-           (d/empty-db {:publication/key {:db/unique :db.unique/identity}
-                        :publication/title {:db/unique :db.unique/value}
-                        :publication/author {:db/type :db.type/ref
-                                             :db/cardinality :db.cardinality/many}
-                        :publication/editor {:db/type :db.type/ref
-                                             :db/cardinality :db.cardinality/many}
-                        :author/name {:db/unique :db.unique/identity}
-                        :editor/name {:db/unique :db.unique/identity}})))
+(defn make-conn []
+  (d/conn-from-db
+   (d/empty-db {:publication/key {:db/unique :db.unique/identity}
+                :publication/title {:db/unique :db.unique/value}
+                :publication/author {:db/type :db.type/ref
+                                     :db/cardinality :db.cardinality/many}
+                :publication/editor {:db/type :db.type/ref
+                                     :db/cardinality :db.cardinality/many}
+                :author/name {:db/unique :db.unique/identity}
+                :editor/name {:db/unique :db.unique/identity}})))
 
-(defn add-namespace
-  "Given a keyword-keyed map `m`, with non-namespaced keys, prefix its keys
-  with the namespace `ns`"
-  [m ns]
-  (reduce-kv (fn[acc k v]
-               (assoc acc (keyword ns (name k)) v))
-             {} m))
+(defn ingest-entry! [conn [_ entry]]
+  (d/transact! conn [entry]))
 
-(defn ingest-entry! [db [entry-key entry]]
-  (let [ns-entry (-> (add-namespace entry "publication")
-                     (assoc :publication/key entry-key))]
-    (d/transact! db [ns-entry])))
-
-(defn ingest-bibliography! [db biblio]
+(defn ingest-bibliography! [conn biblio]
   (let [errors (atom [])]
     (doseq [entry biblio]
-      (try (ingest-entry! db entry)
+      (try (ingest-entry! conn entry)
            (catch Exception e (swap! errors conj (.getMessage e)))))
     @errors))
 
+(defn all-authors [db]
+  (d/q '[:find [?n ...]
+         :where
+         [?e :publication/author ?a]
+         [?a :author/name ?n]] db))
+
+(defn all-titles [db]
+  (d/q '[:find [?t ...]
+         :where
+         [?e :publication/title ?t]] db))
+
+(defn all-entries [db]
+  (d/q '[:find [(pull ?e [* {:publication/author [:author/name]
+                            :publication/editor [:editor/name]}]) ...]
+         :where
+         [?e :publication/key]] db))
+
+(defn fuzzy-by-title [db pattern]
+  (let [regex (re-pattern (str "(?xi)" pattern))]
+    (d/q '[:find [(pull ?e [* {:publication/author [:author/name]
+                               :publication/editor [:editor/name]}]) ...]
+           :in $ ?p
+           :where
+           [?e :publication/title ?t]
+           [(re-find ?p ?t)]] db regex)))
+
+(defn fuzzy-by-author [db pattern]
+  (let [regex (re-pattern (str "(?xi)" pattern))]
+    (d/q '[:find [(pull ?e [* {:publication/author [:author/name]
+                               :publication/editor [:editor/name]}]) ...]
+           :in $ ?p
+           :where
+           [?e :publication/author ?a]
+           [?a :author/name ?n]
+           [(re-find ?p ?n)]] db regex)))
+
 (comment
 
- (ingest-bibliography! conn fi.varela.clj-bibtex/biblio)
+  (def conn (make-conn) )
 
- (d/q '[:find (pull ?e [*])
-        :where [?e]] @conn)
+  (ingest-bibliography! conn fi.varela.clj-bibtex/biblio)
 
- (d/q '[:find (pull ?e [*])
-        :where
-        [?e :publication/author ?a]
-        [?a :author/name ?n]
-        [(re-find #"Varela" ?n)]]
-      @conn)
- (d/q '[:find ?e ?n
-        :where
-        [?e :author/name ?n]
-        [(re-find #"Tobias" ?n)]] @conn)
+  (all-authors @conn)
 
- )
+  (all-titles @conn)
+
+  (all-entries @conn)
+
+  (fuzzy-by-author @conn "tobias")
+
+  (fuzzy-by-title @conn "manage")
+)
