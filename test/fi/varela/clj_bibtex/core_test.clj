4f40d6251db6694423b66c7e4b4255df96c84162
Martín Varela <martin@varela.fi>
2019-11-20 14:48:27 +0200

    dir structure

--- test/fi/varela/clj_bibtex/core_test.clj
+++ test/fi/varela/clj_bibtex/core_test.clj
@@ -1,6 +1,6 @@
-(ns fi.varela.bibtex-test
+(ns fi.varela.clj-bibtex.core-test
   (:require [clojure.test :refer :all]
-            [fi.varela.bibtex :refer :all]))
+            [fi.varela.clj-bibtex.core :as sut]))
 
 (deftest a-test
   (testing "FIXME, I fail."
